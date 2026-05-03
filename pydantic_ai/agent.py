08c04c6d5dd985b899a0d3cf11e8e3a894a20b22
cluiverto <micosoc@protonmail.com>
2026-05-03 22:58:36 +0200

    Agents -> Journey start with this!

---
+++ pydantic_ai/agent.py
@@ -0,0 +1,12 @@
+from pydantic_ai import Agent
+
+
+import os , sys
+# Pobierz katalog główny bam wzgledem obecnego folderu roboczego
+bam_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
+sys.path.insert(0, bam_dir)
+from config import get_openrouter_model
+
+
+agent = Agent(get_openrouter_model(), instructions='You always respond in Italian.')
+agent.to_cli_sync()
