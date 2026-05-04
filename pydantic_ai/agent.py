d75aafed9a1dddcca73426b55e8155ce8fbe0ad5
cluiverto <micosoc@protonmail.com>
2026-05-04 22:24:18 +0200

    feat: add langfuse monitoring and coingecko mcp

--- pydantic_ai/agent.py
+++ pydantic_ai/agent.py
@@ -1,12 +1,36 @@
 from pydantic_ai import Agent
+from pydantic_ai.mcp import MCPServerStreamableHTTP
 
-
-import os , sys
-# Pobierz katalog główny bam wzgledem obecnego folderu roboczego
-bam_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
-sys.path.insert(0, bam_dir)
+from pathlib import Path
+import os, sys
+from dotenv import load_dotenv
+load_dotenv()
+sys.path.insert(0, str(Path(__file__).parent.parent))
 from config import get_openrouter_model
+from langfuse import get_client, propagate_attributes
 
 
-agent = Agent(get_openrouter_model(), instructions='You always respond in Italian.')
+
+# 1. Najpierw instrumentacja (przed utworzeniem agenta)
+Agent.instrument_all()
+
+server = MCPServerStreamableHTTP(os.getenv("ALPHAVANTAGE_URL"))
+
+# 2. Agent z instrument=True
+agent = Agent(
+    get_openrouter_model(), 
+    toolsets=[server],
+    instructions='You always respond in Italian.',
+    instrument=True
+)
+
+# 3. Uruchom z metadanymi
+langfuse = get_client()
+with propagate_attributes(
+    user_id="user_123",
+    session_id="session_abc",
+    tags=["italian-agent"],
+):
+    agent.to_cli_sync()
+
 agent.to_cli_sync()
