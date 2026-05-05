a868a110fbb2246595f0266cfb2b22a21b87ad39
cluiverto <micosoc@protonmail.com>
2026-05-05 19:46:45 +0200

    feat: Simplest form

--- deep-agents/agent.py
+++ deep-agents/agent.py
@@ -1,12 +1,12 @@
-from pydantic_deep import create_deep_agent, DeepAgentDeps, StateBackend
-from pydantic_ai.mcp import MCPServerStreamableHTTP
-from dotenv import load_dotenv
 import os , sys
-
-# Pobierz katalog główny bam wzgledem obecnego folderu roboczego
 bam_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
 sys.path.insert(0, bam_dir)
 from config import get_openrouter_model
+
+from pydantic_deep import create_deep_agent, DeepAgentDeps, StateBackend
+
+
+
 
 agent = create_deep_agent(
     model=get_openrouter_model(),
